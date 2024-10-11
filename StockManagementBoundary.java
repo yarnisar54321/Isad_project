75080c53db316c960eb8cff0c73031b2d30871ce
Yarnisar Jantasare <144825492+yarnisar54321@users.noreply.github.com>
2024-10-11 16:47:49 +0700

    Add files via upload

---
+++ StockManagementBoundary.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proj;
+
+/**
+ *
+ * @author Yarnisar
+ */
+public class StockManagementBoundary {
+//    private StockItem stockItem;
+    
+    public void displayLoginPage(){
+        
+    }
+    
+    public void displayDashboard(){
+        
+    }
+    
+    public void displayStockData(StockItem stockItem){
+        
+    }
+    public void displayTransactionHistory(TransactionHistory history){
+        
+    }
+    
+    public void displayErrorMessage(String message){
+        
+    }
+}
---
+++ stockManager.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proj;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Yarnisar
+ */
+public class stockManager{
+
+    public boolean login(String username, String password){
+       return false;
+    }
+    public void requestStockAdjustment(){
+        
+    }
+    public void requestStockAddition(){
+        
+    }
+    public void requestTransactionRecording(){
+        
+    }
+}
+
