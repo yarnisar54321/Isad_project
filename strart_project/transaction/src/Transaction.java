e768a47c8f34561557e85f8ae5e77ba0684b60e5
aun.kk <66070011@kmitl.ac.th>
2024-10-11 15:14:50 +0700

    Update Transaction.java

--- strart_project/transaction/src/Transaction.java
+++ strart_project/transaction/src/Transaction.java
@@ -6,6 +6,38 @@ public class Transaction {
     private int quantity;
     private Date transactionDate;
     
+    public void setTransactionID(int transactionID){
+        this.transactionID = transactionID;
+    }
+    
+    public void setItemName(String itemName){
+        this.itemName = itemName;
+    }
+    
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+    
+    public void setTransactionDate(Date transactionDate){
+        this.transactionDate = transactionDate;
+    }
+    
+    public int getTransaction(){
+        return this.transactionID;
+    }
+    
+    public String getItemName(){
+        return this.itemName;
+    }
+    
+    public int getQuantity(){
+        return this.quantity;
+    }
+    
+    public Date getTransactionDate(){
+        return this.transactionDate;
+    }
+    
     public String getTransactionDetails(){
         return "none";
     }
