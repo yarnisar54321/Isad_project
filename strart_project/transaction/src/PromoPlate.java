aff52940b287bfb0a290d99cf18593abb4038238
aun.kk <66070011@kmitl.ac.th>
2024-10-12 22:46:18 +0700

    Create PromoPlate.java

---
+++ strart_project/transaction/src/PromoPlate.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import decorClass.RoundedPanel;
+
+public class PromoPlate {
+    private RoundedPanel detailsPlate;
+    private RoundedPanel image;
+    private JTextField nameField, sDateField, eDateField;
+    private JTextArea description;
+    private JComboBox 
+}
